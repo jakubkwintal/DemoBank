594a9e421d3c09bcc29c661ed9e0f0643f813fb8
Jakub Kwintal <jakubkwintal@yahoo.com>
2025-11-09 16:22:58 +0100

    added POM for login

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,20 +1,22 @@
 import { test, expect } from '@playwright/test';
-import { loginData, userId } from '../test-data/login.data';
+import { LoginPage } from '../pages/login.page';
+import { loginData } from '../test-data/login.data';
 
 test.describe('User login to Demobank', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
   });
-  test('successful login with correct credentials', async ({ page }) => {
+  test.only('successful login with correct credentials', async ({ page }) => {
     // Arrange
     const userId = loginData.userId;
     const userPassword = loginData.userPassword;
     const expectedUserName = 'Jan Demobankowy';
 
     // Act
-    await page.getByTestId('login-input').fill(userId);
-    await page.getByTestId('password-input').fill(userPassword);
-    await page.getByTestId('login-button').click();
+    const loginPage = new LoginPage(page);
+    await loginPage.loginInput.fill(userId);
+    await loginPage.passwordInput.fill(userPassword);
+    await loginPage.loginButton.click();
 
     // Assert
     await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
--- tests/payment.spec.ts
+++ tests/payment.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { loginData } from '../test-data/login.data';
 import { assert } from 'console';
+import { LoginPage } from '../pages/login.page';
 
 test.describe('Payment tests', () => {
   test.beforeEach(async ({ page }) => {
@@ -8,9 +9,10 @@ test.describe('Payment tests', () => {
     const userPassword = loginData.userPassword;
 
     await page.goto('/');
-    await page.getByTestId('login-input').fill(userId);
-    await page.getByTestId('password-input').fill(userPassword);
-    await page.getByTestId('login-button').click();
+    const loginPage = new LoginPage(page);
+    await loginPage.loginInput.fill(userId);
+    await loginPage.passwordInput.fill(userPassword);
+    await loginPage.loginButton.click();
     await page.getByRole('link', { name: 'płatności' }).click();
   });
 
